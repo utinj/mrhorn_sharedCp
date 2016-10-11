da0e3d00459e0fc6179a3a6cf8cb40fe69d87d08
Brandon Horn <bhorn@mlschools.org>
2016-10-11 12:47:48 -0400

    initial commit / added chapter_02 source folder

---
+++ Horn, Brandon - shared (CP)/chapter02/KeyboardInputDemo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class KeyboardInputDemo
+{
+
+    public static void main(String[] args)
+    {
+        Scanner fromKeyboard = new Scanner(System.in);
+        
+        System.out.print("What is your name?: ");
+        String name = fromKeyboard.nextLine();
+        
+        System.out.print("What is your favorite color?: ");
+        String color = fromKeyboard.nextLine();
+        
+        System.out.print("What is the airspeed velocity of an unladded swallow?: ");
+        double airspeedVelocity = fromKeyboard.nextDouble();
+        fromKeyboard.nextLine();
+        
+        System.out.print("What is your quest?: ");
+        String quest = fromKeyboard.nextLine();
+        
+        System.out.println("Name: " + name);
+        System.out.println("Color: " + color);
+        System.out.println("Velocity: " + airspeedVelocity);
+        System.out.println("Quest: " + quest);
+    }
+
+}
