2c9bd91c34be2a6706986598576d2157d8750c4b
Brandon Horn <bhorn@mlschools.org>
2016-10-27 10:10:24 -0400

    added Finch stuff. reordered source folders.

---
+++ Horn, Brandon - shared (CP)/finch/template/FinchTemplateFile.java
@@ -0,0 +1,15 @@
+package template;
+
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+public class FinchTemplateFile
+{
+    public static void main(final String[] args)
+    {
+        Finch myFinch = new Finch();
+
+        // Write some code here!
+
+        myFinch.quit();
+    }
+}
