5daaa79a74d9d45b3bc81bc8370f103f213e0ad8
Brandon Horn <bhorn@mlschools.org>
2016-11-07 12:01:37 -0500

    initial commit

--- Horn, Brandon - shared (CP)/chapter03/passByValueExample/PassByValueExample.java
+++ Horn, Brandon - shared (CP)/chapter03/passByValueExample/PassByValueExample.java
@@ -6,18 +6,16 @@ public class PassByValueExample
     public static void main(String[] args)
     {
         int val = 7;
-        
-        System.out.println("val (before): " + val);
+        System.out.println("val (before method call): " + val);
         
         mystery(val);
-        
-        System.out.println("val (after): " + val);
+        System.out.println("val (after method call): " + val);
     }
 
     public static void mystery(int num)
     {
+        System.out.println("num (before set): " + num);
         num = 11;
-        
-        System.out.println("num: " + num);
+        System.out.println("num (after set): " + num);
     }
 }
