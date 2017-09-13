f986d27d1bcbc3dfed1ce38ec7edc08b698c097a
Brandon Horn <bhorn@mlschools.org>
2017-09-13 08:34:33 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/chapter02/eNotation/Enotation.java
@@ -0,0 +1,12 @@
+package eNotation;
+
+public class Enotation
+{
+
+    public static void main(String[] args)
+    {
+        double a = 2.4791E20;
+        System.out.println(a);
+    }
+
+}
---
+++ Horn, Brandon - shared (CP)/chapter02/floatDemo/FloatDemo.java
@@ -0,0 +1,13 @@
+package floatDemo;
+
+public class FloatDemo
+{
+
+    public static void main(String[] args)
+    {
+        float a = 29.95F;
+        
+        System.out.println(a);
+    }
+
+}
