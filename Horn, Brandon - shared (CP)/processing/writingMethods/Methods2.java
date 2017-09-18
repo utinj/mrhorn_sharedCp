ff20ff4e5352d74d8b5e0c13d913cd41fc2feae9
Brandon Horn <bhorn@mlschools.org>
2017-09-18 11:15:51 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/processing/writingMethods/Methods2.java
@@ -0,0 +1,30 @@
+package writingMethods;
+
+import processing.core.PApplet;
+
+public class Methods2 extends PApplet
+{
+    public void setup()
+    {
+        size(500, 500);
+    }
+    
+    public void draw()
+    {
+        background(0);
+        
+        drawSmileyFace();
+    }
+
+    public void drawSmileyFace()
+    {
+        fill(255, 255, 0);
+        ellipse(250, 250, 200, 200);
+        
+        fill(255, 0, 255);
+        ellipse(200, 200, 50, 50);
+        ellipse(300, 200, 50, 50);
+        
+        arc(250, 300, 150, 75, radians(180), radians(359));
+    }
+}
