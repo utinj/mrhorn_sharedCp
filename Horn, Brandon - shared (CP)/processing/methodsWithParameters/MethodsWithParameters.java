2ad4e3ead5da4301ef6cd579e5737f3d5ad6293c
Brandon Horn <bhorn@mlschools.org>
2017-09-19 08:33:47 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/processing/methodsWithParameters/MethodsWithParameters.java
@@ -0,0 +1,32 @@
+package methodsWithParameters;
+
+import processing.core.PApplet;
+
+public class MethodsWithParameters extends PApplet
+{
+    public void setup()
+    {
+        size(1000, 1000);
+    }
+    
+    public void draw()
+    {
+        background(0);
+        
+        drawSmileyFace(250, 250);
+        drawSmileyFace(750, 250);
+        drawSmileyFace(500, 750);
+    }
+    
+    public void drawSmileyFace(int x, int y) // x and y are of the large ellipse
+    {
+        fill(0, 155, 255);
+        ellipse(x, y, 450, 450);
+        
+        fill(255);
+        ellipse(x - 100, y - 100, 50, 50);
+        ellipse(x + 100, y - 100, 50, 50);
+        
+        arc(x, y + 100, 150, 100, radians(0), radians(180));
+    }
+}
