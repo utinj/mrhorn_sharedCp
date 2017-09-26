94c53257e24bbed8c5035c3e8e94305aeddd2bd8
Brandon Horn <bhorn@mlschools.org>
2017-09-26 08:31:28 -0400

    added chapter 4 / initial commit

---
+++ Horn, Brandon - shared (CP)/chapter04/comparingChars/ComparingChars.java
@@ -0,0 +1,16 @@
+package comparingChars;
+
+public class ComparingChars
+{
+    public static void main(String[] args)
+    {
+        char letter1 = 'b';
+        char letter2 = 'z';
+        char letter3 = 'B';
+        
+        if(letter1 < letter2)
+            System.out.println("hi");
+        else
+            System.out.println("bye");
+    }
+}
