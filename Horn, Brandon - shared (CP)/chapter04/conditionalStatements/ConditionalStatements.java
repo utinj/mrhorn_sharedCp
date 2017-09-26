8266b543294ed9e672ddd9905e923420a3aaeb5b
Brandon Horn <bhorn@mlschools.org>
2017-09-26 08:31:52 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/chapter04/conditionalStatements/ConditionalStatements.java
@@ -0,0 +1,18 @@
+package conditionalStatements;
+
+public class ConditionalStatements
+{
+
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 10;
+        
+        if(a + 3 < b)
+        {
+            System.out.println("a is less than b");
+            System.out.println("and I get my Apple Watch today");
+        }
+    }
+
+}
