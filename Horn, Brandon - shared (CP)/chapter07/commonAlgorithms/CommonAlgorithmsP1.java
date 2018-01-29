6362fd0104b07bc2839af4ae08df0328fb7169ab
Brandon Horn <bhorn@mlschools.org>
2018-01-29 08:27:24 -0500

    initial commit

---
+++ Horn, Brandon - shared (CP)/chapter07/commonAlgorithms/CommonAlgorithmsP1.java
@@ -0,0 +1,70 @@
+package commonAlgorithms;
+
+public class CommonAlgorithmsP1
+{
+
+    public static void main(String[] args)
+    {
+//      - comparing
+        
+//        int[] array1 = {6, 6, 8};
+//        int[] array2 = {6, 7, 8};
+//        
+////        System.out.println(array1 == array2); // this only checks references
+//        
+//        if(array1.length == array2.length)
+//        {
+//            boolean same = true;
+//            
+//            for(int i = 0; i < array1.length; i++)
+//            {
+//                if(array1[i] != array2[i])
+//                {
+//                    same = false;
+//                }
+//            }
+//            
+//            if(same)
+//                System.out.println("arrays are the same");
+//            else
+//                System.out.println("not same");
+//        }
+//        else
+//            System.out.println("not same");
+        
+//        - summing values
+//        int[] arr = {5, 6, 7};
+//        
+//        int sum = 0;
+//        int product = 1;
+//        
+//        for(int i = 0; i < arr.length; i++)
+//        {
+//            sum += arr[i];
+//            product *= arr[i];
+//        }
+//        
+//        System.out.println(sum);
+//        System.out.println(product);
+        
+//        - finding largest/smallest
+//        int[] arr = {8, 5, 3, 9, 11, 2};
+//        //           0  1  2  3  4   5
+//        
+//        int biggest = arr[0];
+//        int indexOfSmallest = 0;
+//        
+//        for(int i = 1; i < arr.length; i++)
+//        {
+//            if(arr[i] > biggest)
+//                biggest = arr[i];
+//            
+//            if(arr[i] < arr[indexOfSmallest])
+//                indexOfSmallest = i;
+//        }
+//        
+//        System.out.println(biggest);
+//        System.out.println(indexOfSmallest);
+    }
+
+}
