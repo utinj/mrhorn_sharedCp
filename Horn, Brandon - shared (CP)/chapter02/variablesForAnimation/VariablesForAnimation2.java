72d3d30f4d2235ec1bfca5ebb7abdc907902256d
Brandon Horn <bhorn@mlschools.org>
2018-09-18 07:57:32 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/chapter02/variablesForAnimation/VariablesForAnimation2.java
@@ -0,0 +1,39 @@
+package variablesForAnimation;
+
+import processing.core.PApplet;
+
+public class VariablesForAnimation2 extends PApplet
+{
+
+    private int smileyX;
+    
+    public static void main(String[] args)
+    {
+        PApplet.main("variablesForAnimation.VariablesForAnimation2");
+    }
+
+    public void settings()
+    {
+        size(800, 500);
+    }
+    
+    public void setup()
+    {
+        smileyX = 150;
+    }
+    
+    public void draw()
+    {
+        background(0, 200, 0);
+        
+        smileyX += 2;
+        
+        fill(255, 0, 255);
+        ellipse(smileyX, 250, 300, 300);
+        
+        fill(0);
+        ellipse(smileyX - 50, 250 - 70, 50, 50);
+        ellipse(smileyX + 50, 250 - 70, 50, 50);
+        arc(smileyX, 250, 200, 200, radians(0), radians(180));
+    }
+}
