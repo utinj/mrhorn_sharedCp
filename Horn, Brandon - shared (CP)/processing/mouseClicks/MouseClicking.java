a52bb5ed1feb8d7da77abe8f12b78a61416b820a
Brandon Horn <bhorn@mlschools.org>
2018-09-27 08:19:43 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/processing/mouseClicks/MouseClicking.java
@@ -0,0 +1,32 @@
+package mouseClicks;
+
+import processing.core.PApplet;
+
+public class MouseClicking extends PApplet
+{
+    public static void main(String[] args)
+    {
+        PApplet.main("mouseClicks.MouseClicking");
+    }
+    
+    public void settings()
+    {
+        size(500, 500);
+    }
+    
+    public void setup()
+    {
+        
+    }
+    
+    public void draw()
+    {
+        
+    }
+    
+    public void mouseClicked()
+    {
+        System.out.println(mouseX);
+        System.out.println(mouseY);
+    }
+}
