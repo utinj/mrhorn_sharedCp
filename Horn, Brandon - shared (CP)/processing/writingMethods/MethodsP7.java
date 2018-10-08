c69108080834691b9a2547ece18d494c66dedf59
Brandon Horn <bhorn@mlschools.org>
2018-10-08 12:26:43 -0400

    initial commit

---
+++ Horn, Brandon - shared (CP)/processing/writingMethods/MethodsP7.java
@@ -0,0 +1,40 @@
+package writingMethods;
+
+import processing.core.PApplet;
+
+public class MethodsP7 extends PApplet
+{
+    public static void main(String[] args)
+    {
+        PApplet.main("writingMethods.MethodsP7");
+    }
+    
+    public void settings()
+    {
+        size(500, 500);
+    }
+    
+    public void setup()
+    {
+        
+    }
+    
+    public void draw()
+    {
+        background(0);
+        
+        drawSmileyFace();
+    }
+    
+    public void drawSmileyFace()
+    {
+        fill(0, 155, 255);
+        ellipse(250, 250, 450, 450);
+        
+        fill(255);
+        ellipse(150, 150, 50, 50);
+        ellipse(350, 150, 50, 50);
+        
+        arc(250, 350, 150, 100, radians(0), radians(180));
+    }
+}
