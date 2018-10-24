7f7cf074332e85c1dbc492c2a72a83e1500b1f62
Brandon Horn <bhorn@mlschools.org>
2018-10-24 08:19:14 -0400

    added guide to for loop structure

--- Horn, Brandon - shared (CP)/chapter05/forLoopDemo/ForLoopDemo.java
+++ Horn, Brandon - shared (CP)/chapter05/forLoopDemo/ForLoopDemo.java
@@ -4,12 +4,13 @@ public class ForLoopDemo
 {
     public static void main(String[] args)
     {
+        // for(initialization; condition; update)
+        //      body
+        
         for(int n = 1; n <= 3; n++)
         {
             System.out.println(Math.pow(n, 2));
         }
-        
-        // same functionality as a while loop:
         
 //        int n = 1;
 //        while(n <= 3)
